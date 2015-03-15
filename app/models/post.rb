ac4d943b2cbbe97b3d878e5f4870fec5dba2ff2f
bryantt23 <bryantt23@gmail.com>
2015-03-15 01:23:30 -0700

    ok

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+    # validates :post, presence: true,
+    validates_length_of :body, :minimum => 2
+
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,7 @@ class User < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
   validates_exclusion_of :age, :in => 0...18
 
+  has_many :posts
+
 
 end
